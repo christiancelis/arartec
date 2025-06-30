77aa97d5018136ce9da9e5c74de1dd4c7b4d693a
christiancelis <73555661+christiancelis@users.noreply.github.com>
2025-06-30 18:07:11 -0500

    pagina inicial completada

--- Arartec/source.js
+++ Arartec/source.js
@@ -27,3 +27,43 @@ a.forEach(function(item, indi){
      })
  })
 
+const track = document.querySelector(".slider-track");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+let currentIndex = 0;
+const visibleItems = 1;
+const itemWidth = 360; // Ancho de .product + margen (200 + 2*10)
+
+next.addEventListener("click", () => {
+  const maxIndex = track.children.length - visibleItems;
+  if (currentIndex < maxIndex) {
+    currentIndex++;
+    updateSlider();
+  }
+});
+
+prev.addEventListener("click", () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateSlider();
+  }
+});
+
+function updateSlider() {
+  track.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+}
+
+
+const desplegable = document.querySelector("#desplegable");
+const menuHamburguesa = document.querySelector(".menu-hamburguesa-button");
+
+menuHamburguesa.addEventListener("click", () => {
+  if (desplegable.classList.contains("desplegado")) {
+    desplegable.classList.remove("desplegado");
+    desplegable.classList.add("oculto");
+  } else {
+    desplegable.classList.remove("oculto");
+    desplegable.classList.add("desplegado");
+  }
+});
